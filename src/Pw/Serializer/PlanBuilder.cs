c565378411eb60734c358e71f8fdd0d1ee735d76
Strelets Evgenii <e.str.94@yandex.ru>
2019-10-22 18:58:14 +0700

    refactoring PlanBuilder

--- src/Pw/Serializer/PlanBuilder.cs
+++ src/Pw/Serializer/PlanBuilder.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Collections.Generic;
 using Pw.Serializer.Plans;
@@ -17,42 +17,64 @@ namespace Pw.Serializer
         {
             if (PrimitivePlanItem.IsPrimitive(descriptor.PropertyType))
             {
-                return new PrimitivePlanItem(accessor, descriptor.PropertyType, descriptor.IsBigEndian);
+                return CreatePrimitivePlan(descriptor, accessor);
             }
 
             if (StringPlanItem.IsString(descriptor.PropertyType))
             {
-                return descriptor.IsDynamicSize
-                    ? new StringPlanItem(accessor, descriptor.IsNameChar)
-                    : new StringPlanItem(accessor, descriptor.IsNameChar, descriptor.Size);
+                return CreateStringPlan(descriptor, accessor);
             }
 
             if (TuplePlanItem.IsTuple(descriptor.PropertyType))
             {
-                return CreateTuple(descriptor.PropertyType, accessor);
+                return CreateTuplePlan(descriptor.PropertyType, accessor);
             }
 
             if (descriptor.IsArray)
             {
-                var elementDescriptor = new PropertyDescriptor(descriptor.ElementType);
-
-                if (descriptor.IsDynamicSize)
-                {
-                    var childPlan = CreatePlan(elementDescriptor, new RootObject(descriptor.ElementType.Name));
-                    return new CollectionPlanItem(accessor, descriptor.PropertyType, childPlan);
-                }
-                else
-                {
-                    var elementsPlans = Enumerable.Range(0, descriptor.Size)
-                        .Select(index => new ArrayValueAccessor(index))
-                        .Select(arrayAccessor => CreatePlan(elementDescriptor, arrayAccessor))
-                        .ToArray();
-
-                    return new ArrayPlanItem(accessor, descriptor.PropertyType, elementsPlans);
-                }
+                return CreateArrayPlan(descriptor, accessor);
             }
 
             return CreateComplex(descriptor.PropertyType, accessor);
+        }
+
+        private IPlanItem CreateArrayPlan(PropertyDescriptor descriptor, IValueAccessor accessor)
+        {
+            var elementDescriptor = new PropertyDescriptor(descriptor.ElementType);
+
+            return descriptor.IsDynamicSize
+                ? CreateDynamicArrayPlan(descriptor, accessor, elementDescriptor)
+                : CreateStaticArrayPlan(descriptor, accessor, elementDescriptor);
+        }
+
+        private IPlanItem CreateDynamicArrayPlan(PropertyDescriptor descriptor, IValueAccessor accessor,
+            PropertyDescriptor elementDescriptor)
+        {
+            var childPlan = CreatePlan(elementDescriptor, new RootObject(descriptor.ElementType.Name));
+            return new CollectionPlanItem(accessor, descriptor.PropertyType, childPlan);
+        }
+
+        private IPlanItem CreateStaticArrayPlan(PropertyDescriptor descriptor, IValueAccessor accessor,
+            PropertyDescriptor elementDescriptor)
+        {
+            var elementsPlans = Enumerable.Range(0, descriptor.Size)
+                .Select(index => new ArrayValueAccessor(index))
+                .Select(arrayAccessor => CreatePlan(elementDescriptor, arrayAccessor))
+                .ToArray();
+
+            return new ArrayPlanItem(accessor, descriptor.PropertyType, elementsPlans);
+        }
+
+        private static IPlanItem CreateStringPlan(PropertyDescriptor descriptor, IValueAccessor accessor)
+        {
+            return descriptor.IsDynamicSize
+                ? new StringPlanItem(accessor, descriptor.IsNameChar)
+                : new StringPlanItem(accessor, descriptor.IsNameChar, descriptor.Size);
+        }
+
+        private static IPlanItem CreatePrimitivePlan(PropertyDescriptor descriptor, IValueAccessor accessor)
+        {
+            return new PrimitivePlanItem(accessor, descriptor.PropertyType, descriptor.IsBigEndian);
         }
 
         private IPlanItem CreateComplex(Type type, IValueAccessor accessor)
@@ -70,7 +92,7 @@ namespace Pw.Serializer
             return new ComplexPlan(plan, type, accessor);
         }
 
-        private IPlanItem CreateTuple(Type tupleType, IValueAccessor accessor)
+        private IPlanItem CreateTuplePlan(Type tupleType, IValueAccessor accessor)
         {
             var childs = new List<IPlanItem>();
 
@@ -90,7 +112,6 @@ namespace Pw.Serializer
         {
             return new PrimitivePlanItem(new RootObject("Length"), typeof(int), false); //todo check endian logic
         }
-
 
         private PropertyDescriptor[] GetDescriptors(Type type)
         {
