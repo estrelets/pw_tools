3cb27ecdca2bdf68fe12b330011170e6ae108e02
Strelets Evgenii <e.str.94@yandex.ru>
2020-04-28 19:56:36 +0300

    removed Bootstrapper

--- src/Tools/Pw.Bootstrapper/Tasks/TaskBase.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Pw.Bootstrapper.Tasks
-{
-    public abstract class TaskBase
-    {
-        public virtual Dictionary<string, string> EnvironmentArguments { get; set; }
-    }
-}
